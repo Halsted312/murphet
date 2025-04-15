ccffaded2295bf08375322910f64da005e0d2349
Stephen Murphy <100381493+Halsted312@users.noreply.github.com>
2025-04-15 04:56:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="murphet",
-    version="0.1.0",
+    version="0.1.3",
     author="Stephen Murphy",
     author_email="stephenjmurph@gmail.com",
     description="A Bayesian time-series model for churn rates with changepoints and seasonality",
